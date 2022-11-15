03f280451eb17beaf265a87a67215daa75b294e2
szabomarcel <szabomarcell34@gmail.com>
2022-11-15 11:45:41 +0100

    2022.11.15.

--- python/python 4.9/4.py
+++ python/python 4.9/4.py
@@ -3,3 +3,24 @@ import turtle
 screen = turtle.Screen()
 teknos = turtle.Turtle()
 
+def négyzet(oldal):
+    teknos.forward(oldal / 2)
+    teknos.left(90)
+    teknos.forward(oldal / 2)
+    teknos.left(90)
+    teknos.forward(oldal)
+    teknos.left(90)
+    teknos.forward(oldal)
+    teknos.left(90)
+    teknos.forward(oldal)
+    teknos.left(90)
+    teknos.forward(oldal / 2)
+    teknos.left(90)
+    teknos.forward(oldal / 2)
+
+for i in range(24): 
+    négyzet(50)
+    teknos.left(15)
+
+teknos.hideturtle()
+screen.mainloop()
