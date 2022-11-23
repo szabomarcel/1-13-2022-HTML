35091bb93239d9ea16df458f5493029342ff6079
szabomarcel <szabomarcell34@gmail.com>
2022-11-23 11:58:12 +0100

    2022.11.23. otthoni gyakorlás

---
+++ Python NSÁ/Gyakorlás/12.py
@@ -0,0 +1,6 @@
+szam1 = int(input("Írja be az első számot"))
+szam2 = int(input("Írja be a második számot"))
+if szam1 > szam2:
+    print(szam2, szam1)
+else:
+    print(szam1,szam2)
---
+++ Python NSÁ/Gyakorlás/13.py
@@ -0,0 +1,5 @@
+szam = int(input("20-nál kisebb megadott szám: "))
+if szam >= 20:
+    print("Túl magas")
+else:
+    print("Köszönöm")
