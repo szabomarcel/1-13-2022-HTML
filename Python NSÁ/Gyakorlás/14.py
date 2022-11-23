c71cbeb2bf6f9daad87656a4273d014460eb81b7
szabomarcel <szabomarcell34@gmail.com>
2022-11-23 20:28:08 +0100

    2022.11.23. otthoni gyakorlás

---
+++ Python NSÁ/Gyakorlás/14.py
@@ -0,0 +1,5 @@
+szam = int(input("10 és 20 közötti számokat: "))
+if szam >= 10 and szam <= 20:
+    print("Köszönöm")
+else:
+    print("Helytelen válasz")
---
+++ Python NSÁ/Gyakorlás/15.py
@@ -0,0 +1,5 @@
+szín = input("Írja be a kedvenc színét: ")
+if szín == "piros" or szín == "RED" or szín == "Red":
+    print("Tetszik a piros")
+else:
+    print("Nem szeretem ezt a színt, jobban szeretem a pirost")
