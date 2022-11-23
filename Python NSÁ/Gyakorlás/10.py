469433ec9d17df17d38fc6c1d2e6e394175c810b
szabomarcel <szabomarcell34@gmail.com>
2022-11-23 11:48:30 +0100

    2022.11.23.

---
+++ Python NSÁ/Gyakorlás/10.py
@@ -0,0 +1,3 @@
+kg = int(input("Írja be kilo:"))
+pound = kg * 2.204
+print("Ez a",pound,"pounds")
---
+++ Python NSÁ/Gyakorlás/11.py
@@ -0,0 +1,4 @@
+nagy = int(input("Adjon meg egy számot 100 felett: "))
+kicsi = int(input("Adjon meg egy számot 10 alatt: "))
+válasz = nagy//kicsi
+print(kicsi,"belemegy",nagy, válasz,"idő")
