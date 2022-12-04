926ad34029ae3952e887ec91694cfbc332191b7b
szabomarcel <szabomarcell34@gmail.com>
2022-12-04 20:20:44 +0100

    ,

---
+++ Python NSÁ/karácsony.py
@@ -0,0 +1,5 @@
+print1 = int(input("Írd be az első szót: "))
+print2 = int(input("Írd be a második szót: "))
+print3 = int(input("Írd be a armadik szót: "))
+for print1,print2,print3 in list:
+    print(print1 + print2 + print3)
--- python KAR/python 3.8/7. feladat; 8.deladat;.py
+++ python KAR/python 3.8/7. feladat; 8.deladat;.py
@@ -1,4 +1,4 @@
-importturtle
+import turtle
 
 screen= turtle.Screen()
 
