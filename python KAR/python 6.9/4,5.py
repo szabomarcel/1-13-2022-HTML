c5eb20dbc580e2b88a611696458c5d2a9239328e
szabomarcel <szabomarcell34@gmail.com>
2022-12-05 11:33:08 +0100

    órai feladat

---
+++ python KAR/python 6.9/4,5.py
@@ -0,0 +1,24 @@
+napok = ["Hétfő", "Kedd", "Szerda", "Csütörtök", "Péntek", "Szombat", "Vasárnap"]
+napIndex = 0
+
+def NyaraloSzamolas(napNev, napQ):
+    napIndex = 0
+    for i in range(0, len(napok)):
+        if napNev == napok[i].lower():
+            napIndex = i
+    if napQ < 0:
+        for i in range(0. abs(napQ)):
+            if napIndex == 0:
+                napIndex = 6
+            else:
+                napIndex -= 1
+    else:
+        for i in range(0, napQ):
+            if napIndex == 6:
+                napIndex = 0
+            else:
+                napIndex += 1
+    return napok[napIndex]
+
+    nyaraloNap = NyaraloSzamolas(input("Írd be a mai napot: "), int(input("Hány nap, úlva mész nyaralni?: ")))
+    print(f"{nyaraloNap}-n fogsz menni nyaralni!: ")
--- python KAR/python 6.9/4.py
+++
@@ -1,6 +0,0 @@
-def napok_hozzadas():
-    print(napok_hozzadas("Hétfő",4) == "Péntek")
-    print(napok_hozzadas("Kedd",0) == "Kedd")
-    print(napok_hozzadas("Kedd",14) == "Kedd")
-    print(napok_hozzadas("Hétfő",100) == "Kedd")
-print("Írd ki a napot: ")
