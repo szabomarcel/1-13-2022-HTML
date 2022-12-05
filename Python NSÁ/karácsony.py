c852cab8b6a047c6e4001b84c46c021d5a4870b9
szabomarcel <szabomarcell34@gmail.com>
2022-12-05 10:30:00 +0100

    .

--- Python NSÁ/karácsony.py
+++ Python NSÁ/karácsony.py
@@ -1,5 +1,10 @@
-print1 = int(input("Írd be az első szót: "))
-print2 = int(input("Írd be a második szót: "))
-print3 = int(input("Írd be a armadik szót: "))
-for print1,print2,print3 in list:
-    print(print1 + print2 + print3)
+#Mikulás napra!
+
+szöveg1 = int(input("Írd be az első szót: "))
+szöveg2 = int(input("Írd be a második szót: "))
+szöveg3 = int(input("Írd be a armadik szót: "))
+print(szöveg1 + szöveg2 + szöveg3)
+
+szöveg3 = print("napot")
+szöveg1 = print("Boldog")
+szöveg2 = print("Mikulás")
