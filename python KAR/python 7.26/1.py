f6b5fee1aa1c8e781db62a059f102b9cf8dabe9b
szabomarcel <szabomarcell34@gmail.com>
2022-12-12 12:35:49 +0100

    feladatok

---
+++ python KAR/python 7.26/1.py
@@ -0,0 +1,9 @@
+def paratlan():
+    tömb = [1,3,5,7,9]
+    a = 0
+    for i in tömb:
+        if i % 2 == 1:
+            a = a + 1 
+    print(a)
+print(paratlan())
+
---
+++ python KAR/python 7.26/2.py
@@ -0,0 +1,8 @@
+def osszeadas():
+    tömb = [1,2,3,4,5,6,7,8,9]
+    a = 0
+    for i in tömb:
+        if i % 2 == 0:
+            a = a + i
+    print(a)
+print(osszeadas())
