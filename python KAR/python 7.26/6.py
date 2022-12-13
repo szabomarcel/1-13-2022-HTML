52673c4966c91d9f965b3ac8a4b2071758aac6e3
szabomarcel <szabomarcell34@gmail.com>
2022-12-13 10:30:09 +0100

    feladat

--- python KAR/python 7.26/6.py
+++ python KAR/python 7.26/6.py
@@ -1,2 +1,10 @@
 def szavak():
-    tömb = []
+    tömb = ["van", "nincs", "nem", "igen", "kincs", "ami",]
+    szavak = []
+    for i in tömb:
+        if i != "igen":
+            szavak.append(i)
+        else:
+            return szavak
+    print(szavak)
+print(szavak())
