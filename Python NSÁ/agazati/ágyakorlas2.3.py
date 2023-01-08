e304669e634802bba5c01f46e60b8062b986ecb8
szabomarcel <szabomarcell34@gmail.com>
2023-01-08 20:04:16 +0100

    gyakorlás

---
+++ Python NSÁ/agazati/ágyakorlas2.3.py
@@ -0,0 +1,24 @@
+import random
+
+class Szere:
+    def __init__(self, neve, foglalkozas, nem, szam):
+        self.neve = neve
+        self.foglalkozas = foglalkozas
+        self.nem = nem
+        self.szam = szam
+
+    def nem(nem):
+        if nem == "f":
+            return "Férfi"
+        elif nem == "n":
+            return "Nő"
+
+t = []
+for x in range(3):
+    a = input("Add meg a nevét: ")
+    b = input("Add meg a foglalkozását: ")
+    c = input("Add meg a nemét: ")
+    d = random.randint(1,50)
+    t.append(Szere(a,b,c,d))
+for x in range(3):
+    print(t[x].nev,"")
