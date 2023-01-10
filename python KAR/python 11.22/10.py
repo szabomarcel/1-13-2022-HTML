8a162c060a26aadfa8ba7213785e29a16168e19d
szabomarcel <szabomarcell34@gmail.com>
2023-01-10 20:00:25 +0100

    feladat

--- python KAR/python 11.22/10.py
+++ python KAR/python 11.22/10.py
@@ -5,4 +5,13 @@ teszt(cserel(s, "öm", "om") ==
 "Kerek a gomb, gombszer˝u!")
 
 teszt(cserel(s, "o", "ö") ==
-"Kerek a gömb, gömbszer˝u!")
+"Kerek a gömb, gömbszerű!")
+
+def cserel(s, i):
+    cserel = []
+    for i in range(0,len(s)):
+        if s == i:
+            cserel.append = s["regi"] == i["uj"]
+        return True
+    for x in range(0,len(s)):
+        print(cserel[i].s,"")
