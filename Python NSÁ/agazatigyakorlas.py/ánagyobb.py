5f6adc5be7f8bd4becd4437944ccb21b64681d2b
szabomarcel <szabomarcell34@gmail.com>
2023-01-16 15:41:03 +0100

    main

---
+++ Python NSÁ/agazatigyakorlas.py/ánagyobb.py
@@ -0,0 +1,8 @@
+a = int(input("Ítj be az első számot!"))
+b = int(input("Ítj be a második számot!"))
+if a > b:
+    print(a, "nagyobb, mint", b)
+if a < b:
+    print(b, "nagyobb, mint", b )
+else:
+    print("A két szám egyforma")
